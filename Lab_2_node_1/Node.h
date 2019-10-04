0d8b8ca07012cbce6d0233a2e6ee3ffda5f77f0c
Abdullah Saleem <2018cs39@student.uet.edu.pk>
2019-10-04 09:40:23 +0500

    Node class updated to Doubly Node class

--- Lab_2_node_1/Node.h
+++ Lab_2_node_1/Node.h
@@ -2,11 +2,15 @@
 class Node
 {
 	int value;
+	Node* prevNode;
 	Node* nextNode;
 public:
+	Node();
 	void setValue(int v);
+	void setPrevNode(Node* nn);
 	void setNextNode(Node* nn);
 	int getValue();
+	Node* getPrevNode();
 	Node* getNextNode();
 	
 };
