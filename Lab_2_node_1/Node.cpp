2944c4075d250cdeb0b8b49f70cd8dcbc2708e8b
Abdullah Saleem <2018cs39@student.uet.edu.pk>
2019-10-04 09:43:07 +0500

    Definations of functions after updating Node class

--- Lab_2_node_1/Node.cpp
+++ Lab_2_node_1/Node.cpp
@@ -1,10 +1,21 @@
 #include "node.h"
 
 
+Node::Node()
+{
+	value = 0;
+	prevNode = nextNode = 0;
+}
+
 // function for setting value of node
 void Node::setValue(int v)
 {
 		value = v;
+}
+
+void Node::setPrevNode(Node* nn)
+{
+	prevNode = nn;
 }
 
 void Node::setNextNode(Node* nn)
@@ -15,6 +26,11 @@ void Node::setNextNode(Node* nn)
 int Node::getValue()
 {
 	return value;
+}
+
+Node* Node::getPrevNode()
+{
+	return prevNode;
 }
 
 Node* Node::getNextNode()
